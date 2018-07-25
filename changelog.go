9d95908c6cca318ec33a3343b3013087931e8869
Peter Booker <mail@peterbooker.com>
2018-07-25 16:09:37 +0100

    improve changelog now returns slug and revision for each change

--- changelog.go
+++ changelog.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"regexp"
-	"strings"
 )
 
 const (
@@ -14,12 +13,14 @@ const (
 
 var (
 	// TODO: Update this to html parsing?
-	regexChangelog = regexp.MustCompile(`[a-z]+-[a-z]+\/`)
+	regexChangelog2 = regexp.MustCompile(`[a-z]+-[a-z]+\/`)
+	regexChangelog1 = regexp.MustCompile(`(?s)\[(.+?)\].+?\* (.+?)\/(?:tags\/)?(.+?)\/`)
+	regexChangelog  = regexp.MustCompile(`(?s)\[(.+?)\].+?\* (.+?)\/`)
 )
 
 // GetChangeLog fetches a list of updated Plugins/Themes from between the provided revisions
-func (c *Client) GetChangeLog(dir string, current, latest int) ([]string, error) {
-	var list []string
+func (c *Client) GetChangeLog(dir string, current, latest int) ([][]string, error) {
+	var list [][]string
 	log.Printf("Current: %d Latest: %d\n", current, latest)
 
 	for current < latest {
@@ -42,11 +43,14 @@ func (c *Client) GetChangeLog(dir string, current, latest int) ([]string, error)
 	}
 	list = append(list, items...)
 
+	// Reverse array so lowest revision is first
+	list = reverseList(list)
+
 	return list, err
 }
 
-func (c *Client) doChangeLog(URL string) ([]string, error) {
-	var list []string
+func (c *Client) doChangeLog(URL string) ([][]string, error) {
+	var list [][]string
 
 	// Make the Request
 	resp, err := c.getRequest(URL)
@@ -57,17 +61,27 @@ func (c *Client) doChangeLog(URL string) ([]string, error) {
 	// Drain body and check Close error
 	defer drainAndClose(resp.Body, &err)
 	bytes, err := ioutil.ReadAll(resp.Body)
-	matches := regexChangelog.FindAllString(string(bytes), -1)
+
+	matches := regexChangelog.FindAllStringSubmatch(string(bytes), -1)
 
 	found := make(map[string]bool)
 	// Get the desired substring match and remove duplicates
 	for _, match := range matches {
-		match = strings.TrimRight(match, "/")
-		if !found[match] {
-			found[match] = true
-			list = append(list, match)
+		if !found[match[2]] {
+			found[match[2]] = true
+			// Reverse values so slug is first
+			list = append(list, []string{match[2], match[1]})
 		}
 	}
 
 	return list, err
+}
+
+// reverseList reverses the array
+func reverseList(list [][]string) [][]string {
+	last := len(list) - 1
+	for i := 0; i < len(list)/2; i++ {
+		list[i], list[last-i] = list[last-i], list[i]
+	}
+	return list
 }
